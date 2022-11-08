cd863504bb2ff4a74d5bbf7f4b6a729718107b5d
anna sganderla <sganderlakarlaanna@gmail.com>
2022-11-08 20:02:43 -0300

    ajustes no titulo responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,9 @@ const Header = () => {
             <div className={styles.logo}>
                 <BsCurrencyEuro color='white' size={50} />
             </div>
+            <div className={styles.title_area}>
+                <p> <strong>Welcome</strong>  to <strong>your</strong> best web <strong>wallet</strong> </p>
+            </div>
         </header>
     );
 };

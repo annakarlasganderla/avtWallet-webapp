4983b3e1571476461db024fa4c71724bd42ee4e5
VitorDanielAdams <vitor.adams@outlook.com>
2023-03-27 20:30:53 -0300

    fixed - Ajustes nas libs do package.json

--- front-end/src/components/TextField/stories/textField.stories.tsx
+++ front-end/src/components/TextField/stories/textField.stories.tsx
@@ -1,8 +1,8 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import TextField from "../TextField";
 
-type Meta = ComponentMeta<typeof TextField>
-type Story = ComponentStory<typeof TextField>
+type Meta = ComponentMeta<typeof TextField>;
+type Story = ComponentStory<typeof TextField>;
 
 export default {
 	title: "AVT/Componentes/TextField",
@@ -10,25 +10,25 @@ export default {
 } as Meta;
 
 export const Default: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 Default.storyName = "Padrão";
 Default.args = {
 	name: "padrao",
 	type: "text",
 	placeholder: "placeholder",
-}
+};
 
 export const WithLabel: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 WithLabel.storyName = "Com Label";
 WithLabel.args = {
-	label: "Label"
+	label: "Label",
 };
 
 export const Disabled: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 Disabled.storyName = "Desabilitado";
 Disabled.args = {
@@ -39,29 +39,29 @@ Disabled.args = {
 };
 
 export const Error: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 Error.storyName = "Quando Houver Erro";
 Error.args = {
 	label: "Error",
-	error: "Login or password are incorrect!"
+	error: "Login or password are incorrect!",
 };
 
 export const Size: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 Size.storyName = "Quando Houver alterações no tamanho";
 Size.args = {
 	label: "Tamanho",
 	width: 100,
-	height: 50
+	height: 50,
 };
 
 export const TextArea: Story = (args) => {
-	return <TextField {...args} />
+	return <TextField {...args} />;
 };
 TextArea.storyName = "Quando for TextArea";
 TextArea.args = {
 	name: "textarea",
 	label: "TextArea",
-}
+};

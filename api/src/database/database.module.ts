6555aa4271a29c3a61ae5e935f1e1e10aeff6792
VitorAdams <77992662+VitorDanielAdams@users.noreply.github.com>
2023-06-05 21:50:33 -0300

    fix - DB port

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -10,7 +10,7 @@ import { User } from 'src/users/entities/user.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5433,
+      port: 5432,
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',

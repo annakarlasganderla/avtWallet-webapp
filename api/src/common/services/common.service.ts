49acc4ee2547eb0b1509d86c9cdf72000b16cacf
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-06-09 17:22:16 -0300

    fix: update handleError log

--- api/src/common/services/common.service.ts
+++ api/src/common/services/common.service.ts
@@ -2,7 +2,7 @@ import { HttpException, Logger } from '@nestjs/common';
 
 export const handleErrors = (message: string, code: number) => {
   const logger = new Logger();
-  logger.log(message);
+  logger.error(message);
 
   throw new HttpException(message, code);
 };

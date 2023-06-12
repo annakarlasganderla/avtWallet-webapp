1c24cdf9fdb6d87d9b6bfe446064f16a8e0202c0
VitorDanielAdams <vitor.adams@outlook.com>
2023-06-12 20:21:45 -0300

    Feat - Adicionado queryclientprovider

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,10 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
 import { AuthorizationInterceptor } from "./api/Api";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<CookiesProvider>
-		<AuthorizationInterceptor />
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<AuthorizationInterceptor />
+			<App />
+		</QueryClientProvider>
 	</CookiesProvider>,
 );

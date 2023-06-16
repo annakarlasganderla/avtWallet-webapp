c9b8b5d4ef7ab28b91f005f84615315f8ee02a25
VitorDanielAdams <vitor.adams@outlook.com>
2023-06-16 20:42:29 -0300

    Fixed - return Get API de tags e sources

--- front-end/src/api/Tags.ts
+++ front-end/src/api/Tags.ts
@@ -1,46 +1,46 @@
-import { listAll, post, remove, update } from './Common';
-import { TagsDto } from '../types/tags.types';
+import { listAll, post, remove, update } from "./Common";
+import { Tags, TagsDto } from "../types/tags.types";
 
 const TagsApi = () => {
-    const url = '/tags';
+	const url = "/tags";
 
-    const handleError = (error: any) => {
-        return Promise.reject(error.response);
-    };
+	const handleError = (error: any) => {
+		return Promise.reject(error.response);
+	};
 
-    const listAllTags = async () => {
-        try {
-            return await listAll(url);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    }
+	const listAllTags = async (): Promise<Tags[]> => {
+		try {
+			return await listAll(url);
+		} catch (e: any) {
+			return handleError(e);
+		}
+	};
 
-    const postTags = async (obj: TagsDto) => {
-        try {
-            return await post(url, obj);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    }
+	const postTags = async (obj: TagsDto) => {
+		try {
+			return await post(url, obj);
+		} catch (e: any) {
+			return handleError(e);
+		}
+	};
 
-    const updateTag = async (obj: TagsDto, id: string) => {
-        try {
-            return await update(url, obj, id);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    }
+	const updateTag = async (obj: TagsDto, id: string) => {
+		try {
+			return await update(url, obj, id);
+		} catch (e: any) {
+			return handleError(e);
+		}
+	};
 
-    const deleteTag = async (id: string) => {
-        try {
-            return await remove(url, id);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    }
+	const deleteTag = async (id: string) => {
+		try {
+			return await remove(url, id);
+		} catch (e: any) {
+			return handleError(e);
+		}
+	};
 
-    return { listAllTags, postTags, updateTag, deleteTag }
-}
+	return { listAllTags, postTags, updateTag, deleteTag };
+};
 
 export default TagsApi;
